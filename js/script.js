1b57ba861c18f2d82eee29cefff7226f81506c4a
AhsanAkbar252 <ahsanakbar252@gmail.com>
2023-07-25 18:18:21 +0500

    added styling and other sections

--- js/script.js
+++ js/script.js
@@ -28,39 +28,36 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+    // Smooth scrolling when clicking on Navbar links
+    $("a.nav-link").on("click", function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top - 100
+          },
+          800 // Adjust the animation speed (in milliseconds) as needed
+        );
+      }
+    });
 
-    $(document).ready(function() {
-      // Smooth scrolling when clicking on Navbar links
-      $("a.nav-link").on("click", function(event) {
-        if (this.hash !== "") {
-          event.preventDefault();
-          const hash = this.hash;
-          $("html, body").animate(
-            {
-              scrollTop: $(hash).offset().top
-            },
-            800 // Adjust the animation speed (in milliseconds) as needed
-          );
+    // Highlight active nav-link based on scroll position
+    $(window).on("scroll", function() {
+      const scrollPosition = $(window).scrollTop();
+
+      $("div").each(function() {
+        const target = $(this);
+        const id = target.attr("id");
+        const navLink = $('a[href="#' + id + '"]');
+        const offset = target.offset().top - 110;
+        const height = target.outerHeight();
+
+        if (scrollPosition >= offset && scrollPosition < offset + height) {
+          navLink.addClass("active");
+        } else {
+          navLink.removeClass("active");
         }
-      });
-
-      // Highlight active nav-link based on scroll position
-      $(window).on("scroll", function() {
-        const scrollPosition = $(window).scrollTop();
-
-        $("div").each(function() {
-          const target = $(this);
-          const id = target.attr("id");
-          const navLink = $('a[href="#' + id + '"]');
-          const offset = target.offset().top;
-          const height = target.outerHeight();
-
-          if (scrollPosition >= offset && scrollPosition < offset + height) {
-            navLink.addClass("active");
-          } else {
-            navLink.removeClass("active");
-          }
-        });
       });
     });
   });
